3a9ec61d5e2ae8c544b524cdf752e1b16b441bf1
bram <nogrend@msn.com>
2023-12-30 21:47:01 +0100

    initial commit

---
+++ src/main/java/com/wattwatcher/WattWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.wattwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WattWatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wattwatcher/WattWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wattwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WattWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
