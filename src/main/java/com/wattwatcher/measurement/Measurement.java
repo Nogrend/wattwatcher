ee71599913bcbfb364868643cb47511b0a3f40b6
bram <nogrend@msn.com>
2024-01-14 23:35:44 +0100

    clean up

--- src/main/java/com/wattwatcher/measurement/Measurement.java
+++ src/main/java/com/wattwatcher/measurement/Measurement.java
@@ -2,17 +2,15 @@ package com.wattwatcher.measurement;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Entity(name = "measurement")
-@Table(name = "measurements")
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Measurement {
